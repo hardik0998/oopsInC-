c7be26f9fd6f8382359f9de5329b4030258c7521
hardik0998 <77712415+hardik0998@users.noreply.github.com>
2022-06-22 12:58:10 +0530

    destructor created

--- staticAllocation.cpp
+++ staticAllocation.cpp
@@ -14,18 +14,24 @@ h1.name="Hk";
 h1.setHealth(45);
 h1.setLevel('A');
 
-cout<<h1.getHealth()<<endl;
-cout<<h1.getLevel()<<endl;
+// cout<<h1.getHealth()<<endl;
+// cout<<h1.getLevel()<<endl;
 
 hero h2(h1);
 h2.print();
 
+hero h3;
+h3.name="Dk";
+h3.power=80;    
+h3.print();
+h1 = h3;
+h1.print();
+h3.print();
 
 
 
-
-cout<<" size "<<sizeof(h1)<<endl;
-temp t1; // This is  a temporary empty class object;
-cout<<" size "<<sizeof(t1)<<endl;
+// cout<<" size "<<sizeof(h1)<<endl;
+// temp t1;This is  a temporary empty class object;
+// cout<<" size "<<sizeof(t1)<<endl;
 
 }
